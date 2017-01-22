6602442fce83c7c4606a96e2bbad3a4b4a4d7793
Laura Vingada <laura@vingada.com>
2017-01-22 15:36:20 -0500

    valid urls are confimed and I can assign a short id, then return a json of url and short url

--- app.js
+++ app.js
@@ -6,10 +6,29 @@ const shortid = require('shortid');
 //sampling shortid
 console.log(shortid.generate());
 
+// sampling validUrl
+const isValid = 'https://cyclokitty.github.io';
+if (validUrl.isUri(isValid)) {
+  console.log(`${isValid} is fine.`);
+} else {
+  console.log('Nope');
+}
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/:link(*)', (req, res) => {
+  const link = req.params.link;
+  if (validUrl.isUri(link)) {
+    let shortId = shortid.generate();
+    let shortLink = `http://localhost:3000/${shortId}`;
+    res.json({ "valid": `${link}`, "shortId": `${shortLink}` });
+  } else {
+    res.send(`Your link is not valid. Please try again.`);
+  }
 });
 
 const port = process.env.PORT || 3000;
